657db993036e60b99eba9deaaa6dcee3470d6289
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-21 13:07:11 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv()
-TG_TOKEN = os.environ['TELEGRAM_TOKEN']
-bot = telegram.Bot(TG_TOKEN)
-bot.send_message(chat_id="@NASA_AND_SPASE_X", text="Тестировочка")
-print(bot.get_me())
+def main():
+    load_dotenv()
+    TG_TOKEN = os.environ['TELEGRAM_TOKEN']
+    CHAT_ID = os.environ['CHAT_ID']
+    bot = telegram.Bot(TG_TOKEN)
+    bot.send_document(CHAT_ID, document=open('nasa_images/image_38.jpg', 'rb'))
+
+
+if __name__ == "__main__":
+    main()
