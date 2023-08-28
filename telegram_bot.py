88f2f98eafb02d148f687ab831b145ab2ead9ca9
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-28 15:25:15 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,20 +4,26 @@ import os
 import time
 import os
 import random
+import argparse
 
-
-def main():
-    load_dotenv()
+def main(path,seconds):
     bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
-    for root, dirs, files in os.walk('nasa_images'):
+    for root, dirs, files in os.walk(f'{path}'):
         while True:
-            with open(os.path.join(root, random.choice(files)), 'rb') as file:
-                bot.send_document(os.environ['TG_CHAT_ID'], document=file)
-            time.sleep(int(os.environ['FREQ_PUBLISH']))
+            try:
+                with open(os.path.join(root, random.choice(files)), 'rb') as file:
+                    bot.send_document(os.environ['TG_CHAT_ID'], document=file)
+                time.sleep(int(seconds))
+            except telegram.error.NetworkError as err:
+                print("network error")
+                break
     
 if __name__ == "__main__":
-    try:
-        main()
-    except telegram.error.NetworkError as err:
-        print(err)
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default=os.getenv("PATH_TO_FILE"))
+    parser.add_argument('--seconds', default=14400)
+    args = parser.parse_args()
+    main(args.path, args.seconds)
+    
 
