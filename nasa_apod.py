9b08b22c85a964887447cb1f4c113c9a81699e64
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-28 15:24:58 +0300

    Update nasa_apod.py

--- nasa_apod.py
+++ nasa_apod.py
@@ -6,24 +6,22 @@ from dotenv import load_dotenv
 
 
 def fetch_nasa_apod_picture(count):
-    load_dotenv()
     payload = {
-        'api_key': os.environ['DEMO_KEY'],
-        'count': count if count else 50
+        'api_key': os.environ['NASA_API_KEY'],
+        'count': count
         }
     nasa_url = 'https://api.nasa.gov/planetary/apod'
     response = requests.get(nasa_url, params=payload)
     response.raise_for_status()
-    for num_of_picture, dict in enumerate(response.json()):
-        if dict['media_type'] == 'image':
-            download_a_picture(dict['hdurl'], 'nasa_images', num_of_picture)
+    for num_of_picture, dictionary in enumerate(response.json()):
+        if dictionary['media_type'] == 'image':
+            download_a_picture(dictionary['hdurl'], 'nasa_images', num_of_picture)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     parser = argparse.ArgumentParser()
-    parser.add_argument('--count', default="")
+    parser.add_argument('--count', default="50")
     args = parser.parse_args()
-    try:
-        fetch_nasa_apod_picture(args.count)
-    except requests.exceptions.HTTPError as err:
-        print(err)
+    fetch_nasa_apod_picture(args.count)
+
