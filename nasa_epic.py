21dc83cfa91bc8573d09fd434ea3175879fabe68
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-30 15:59:40 +0300

    Update nasa_epic.py

--- nasa_epic.py
+++ nasa_epic.py
@@ -6,34 +6,39 @@ import os
 import argparse
 
 
-def fetch_archive_picture(day, i, dates):
-    if len(dates) > 0:
-        payload = {
-            'api_key': os.environ['NASA_API_KEY'],
-        }
-        nasa_epic_url = 'https://api.nasa.gov/EPIC/archive/natural/'
-        response_nasa2_url = requests.get(f'{nasa_epic_url}{day.strftime("%Y/%m/%d") }/png/{dates[0]["image"]}.png', params=payload)
-        response_nasa2_url.raise_for_status()
-        download_a_picture(response_nasa2_url.url, 'nasa_epic', i)
-
-
-def fetch_nasa_epic_picture(count):
+def fetch_archive_picture(day, number_of_picture, dates, NASA_API_KEY):
     payload = {
-        'api_key': os.environ['NASA_API_KEY'],
+        'api_key': NASA_API_KEY,
+    }
+    nasa_epic_url = 'https://api.nasa.gov/EPIC/archive/natural/'
+    response_nasa = requests.get(f'{nasa_epic_url}{day.strftime("%Y/%m/%d") }/png/{dates[0]["image"]}.png', params=payload)
+    response_nasa.raise_for_status()
+    download_a_picture(response_nasa.url, 'nasa_epic', number_of_picture)
+
+
+def fetch_nasa_epic_picture(count, NASA_API_KEY):
+    payload = {
+        'api_key': NASA_API_KEY,
     }
     nasa_date_url = 'https://api.nasa.gov/EPIC/api/natural/date/'
     today = datetime.date.today()
-    for i in range(count):
+    for number in range(count):
         day = today - datetime.timedelta(days=i)
         response_date = requests.get(f'{nasa_date_url}{day.strftime("%Y-%m-%d")}', params=payload)
         response_date.raise_for_status()
         dates = response_date.json()
-        fetch_archive_picture(day, i, dates)
+        if dates:
+            fetch_archive_picture(day, number, dates, NASA_API_KEY)
 
 
-if __name__ == "__main__":
+def main():
     load_dotenv()
     parser = argparse.ArgumentParser()
     parser.add_argument('--count', default=20)
     args = parser.parse_args()
-    fetch_nasa_epic_picture(args.count)
+    NASA_API_KEY = os.environ['NASA_API_KEY']
+    fetch_nasa_epic_picture(args.count, NASA_API_KEY)
+
+
+if __name__ == "__main__":
+    main()
