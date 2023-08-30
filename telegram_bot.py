54870cad459ad9a58c1e2b5647dd2949d105898f
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-30 15:57:23 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,24 +6,25 @@ import os
 import random
 import argparse
 
-def main(path,seconds):
+def main():
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default=os.environ("PATH_TO_FOLDER"))
+    parser.add_argument('--seconds', default=14400, type=int)
+    args = parser.parse_args()
     bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
-    for root, dirs, files in os.walk(f'{path}'):
+    for root, dirs, files in os.walk(args.path):
         while True:
             try:
                 with open(os.path.join(root, random.choice(files)), 'rb') as file:
                     bot.send_document(os.environ['TG_CHAT_ID'], document=file)
-                time.sleep(int(seconds))
-            except telegram.error.NetworkError as err:
+                time.sleep(args.seconds)
+            except telegram.error.NetworkError:
                 print("network error")
                 break
     
+
 if __name__ == "__main__":
-    load_dotenv()
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--path', default=os.getenv("PATH_TO_FILE"))
-    parser.add_argument('--seconds', default=14400)
-    args = parser.parse_args()
-    main(args.path, args.seconds)
+    main()
     
 
