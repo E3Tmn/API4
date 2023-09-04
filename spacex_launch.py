5a45beed956f73f63e25802c5069ae089657ce4e
E3Tmn <nick1tapinyagin@yandex.ru>
2023-09-04 16:11:47 +0300

    Update spacex_launch.py

--- spacex_launch.py
+++ spacex_launch.py
@@ -3,15 +3,12 @@ import argparse
 from download import download_a_picture
 
 
-def fetch_spacex_launch(launch_id):
+def fetch_spacex_launch(launch_id, launch_number=22):
     spacex_url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     response = requests.get(spacex_url)
     response.raise_for_status()
-    if launch_id:
-        list_of_picture = response.json()['links']['flickr']['original']
-    else:
-        launch_number = 22
-        list_of_picture = response.json()[launch_number]['links']['flickr']['original']
+    launch = response.json() if launch_id else response.json()[launch_number]
+    list_of_picture = launch['links']['flickr']['original']
     for num_of_picture, link_to_the_picture in enumerate(list_of_picture):
         download_a_picture(link_to_the_picture, 'spacex_images', num_of_picture)
 
@@ -25,5 +22,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
- 
