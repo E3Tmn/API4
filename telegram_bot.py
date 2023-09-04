baaad9b6562392ca06967d79a4b0efd4e92e205b
E3Tmn <nick1tapinyagin@yandex.ru>
2023-09-04 16:11:27 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -16,17 +16,24 @@ def main():
     parser.add_argument('--filename', help='Название картинки')
     args = parser.parse_args()
     bot = telegram.Bot(os.environ['TELEGRAM_TOKEN'])
+    b_first_lap = True
+    delay = 1
     while True:
         for root, dirs, files in os.walk(args.path):
+            time.sleep(delay)
+            delay = args.seconds
             try:
-                filename = args.filename if args.filename else random.choice(files)
+                if args.filename and b_first_lap:
+                    filename = args.filename
+                    b_first_lap = False
+                else:
+                    filename = random.choice(files)
                 with open(os.path.join(root, filename), 'rb') as file:
                     bot.send_document(os.environ['TG_CHAT_ID'], document=file)
-                time.sleep(args.seconds)
             except telegram.error.NetworkError:
                 print("network error")
                 break
-        time.sleep(args.seconds)
+
 
 if __name__ == "__main__":
     main()
